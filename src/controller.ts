b7ab0e6e5af1e80db48818906d4f6f0616913b11
Utku Sarioglu <utkusarioglu@hotmail.com>
2019-11-07 09:02:03 +0300

    Exposed constants and more types

--- src/controller.ts
+++ src/controller.ts
@@ -36,7 +36,18 @@ import { t_namespace } from "@utkusarioglu/namespace";
 
 export { M_Controller } from "./m_controller";
 export { M_ControllerEvents } from "./m_controller_events";
-export { t_transmission } from "./t_controller";
+export {
+    e_Scope,
+    t_transmission,
+    t_subscription,
+    t_sequenceStep,
+    t_dependency_group,
+    t_service,
+    t_reception,
+    t_channel,
+    t_waitSet,
+} from "./t_controller";
+export { C_Controller } from "./c_controller";
 
 /**
  * Establishes local and global event emitters,
